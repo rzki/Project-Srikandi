6617624466c81c6d862d4c5e0f0f8652e8a24dc1
rzki <rizkymail14@gmail.com>
2021-08-02 13:25:14 +0800

    Restructure
    - Changing extension from html to php to look cleaner
    - restructure head and footer by creating separate file for head and footer in 'components' folder

---
+++ about.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+    <title>About - Srikandi Handicrafts</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    
+    <? include 'components/head.php'; ?>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html"><img src="assets/img/LOGO SRIKANDI.jpg" alt=""></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="products.html">Products</a></li>
+          <li><a href="catalog.html" class="active">Catalog</a></li>
+          <li><a href="about.html" class="active">About</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>About</h2>
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>About</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row content">
+          <div class="col-lg-6 content-1">
+            <h2>Srikandi Handicraft</h2>
+            <h3>Srikandi Handicraft is a company engaged in the sale and purchase of handicrafts. This company was
+              founded in 2021. The
+              products that are sold are of very good quality.</h3>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 logo-content ">
+            <img class="photo-logo  d-flex justify-content-center" src="assets/img/logo.png" alt="" srcset="">
+            <h3 class="text-center fw-bold judul">Logo Srikandi Handicraft</h3>
+          </div>
+        </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Team</h2>
+          <p>Our Hardowrking Team</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/owner-1.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Nanik Farida</h4>
+                <span>Owner Srikandi Handicraft</span>
+                <p></p>
+                <div class="social">
+                  <!-- <a href=""><i class="ri-twitter-fill"></i></a> -->
+                  <a href="https://www.facebook.com/nanik.farida.9" class="mt-4"><i class="ri-facebook-fill "></i></a>
+                  <!-- <a href=""><i class="ri-instagram-fill"></i></a> -->
+                  <a href="https://wa.me/+6281238205626" class="mt-4"> <i class="ri-whatsapp-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="assets/img/team/owner-2.jpg" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Ninik Yulianti</h4>
+                <span>Owner Srikandi Handicraft</span>
+                <p></p>
+                <div class="social">
+                  <!-- <a href=""><i class="ri-twitter-fill"></i></a> -->
+                  <a href="https://www.facebook.com/ninik.yulianti.1" class="mt-4"><i class="ri-facebook-fill"></i></a>
+                  <!-- <a href=""><i class="ri-instagram-fill"></i></a> -->
+                  <a href="https://wa.me/+6281339679859" class="mt-4"> <i class="ri-whatsapp-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Team Section -->
+  </main>
+  <!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <? include 'components/footer.php';?>
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
